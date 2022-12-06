097a4c1154ae2243e670d3d31dc2a3d49a67a466
Luong Phuc Dien <99782891+luongphucdien@users.noreply.github.com>
2022-12-06 22:52:30 +0700

    modify: Fix user fragment UI

---
+++ HealthCareApp/app/src/main/java/com/example/healthcareapp/PersonalInfoActivity.java
@@ -0,0 +1,50 @@
+package com.example.healthcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+    private TextInputEditText userName, userPhone, userNewPW;
+    private ImageView userPfp;
+    private MaterialButton updateInfoBtn;
+    private MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+
+        userName = findViewById(R.id.userName);
+        userPhone = findViewById(R.id.userPhone);
+        userNewPW = findViewById(R.id.userNewPassword);
+        userPfp = findViewById(R.id.userPfp);
+        updateInfoBtn = findViewById(R.id.updateInfoButton);
+        toolbar = findViewById(R.id.toolbar);
+
+        updateInfoBtn.setOnClickListener(updateInfoHandler);
+
+        toolbar.setNavigationOnClickListener(toolbarOnBackPressedHandler);
+    }
+
+    private View.OnClickListener updateInfoHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO: Add logics to handle checking if info is new and saving to DB
+        }
+    };
+
+    private View.OnClickListener toolbarOnBackPressedHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+}
--- HealthCareApp/app/src/main/java/com/example/healthcareapp/UserFragment.java
+++ HealthCareApp/app/src/main/java/com/example/healthcareapp/UserFragment.java
@@ -1,58 +1,30 @@
 package com.example.healthcareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.card.MaterialCardView;
+
 public class UserFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UserFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
-        UserFragment fragment = new UserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ImageView userPfp;
+    private TextView userName, userPhone;
+    private MaterialButton userLogoutBtn;
+    private MaterialCardView userPersonalInfo, userAbout, userSignOut;
+    private Intent intent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,4 +33,46 @@ public class UserFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        userPfp = view.findViewById(R.id.userPfp);
+        userName = view.findViewById(R.id.userName);
+        userPhone = view.findViewById(R.id.userPhone);
+        userLogoutBtn = view.findViewById(R.id.userLogoutButton);
+        userPersonalInfo = view.findViewById(R.id.userPersonalInfo);
+        userAbout = view.findViewById(R.id.userAbout);
+        userSignOut = view.findViewById(R.id.userSignOut);
+
+        userLogoutBtn.setOnClickListener(signOutHandler);
+
+        userPersonalInfo.setOnClickListener(personalInfoHandler);
+        userAbout.setOnClickListener(aboutHandler);
+        userSignOut.setOnClickListener(signOutHandler);
+    }
+
+    private View.OnClickListener personalInfoHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            intent = new Intent(getContext(), PersonalInfoActivity.class);
+            getActivity().startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener aboutHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            intent = new Intent(getContext(), AboutActivity.class);
+            getActivity().startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener signOutHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO: Add logics to handle signing out
+        }
+    };
 }
