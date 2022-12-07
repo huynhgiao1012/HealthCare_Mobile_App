ff33aed32da9fe56a05ec58e11080451b5fbf3b5
DuongMinhLoc <duongminhloc201@gmail.com>
2022-12-07 18:57:51 +0700

    Loc | api for signin, signup, get news, save news

---
+++ HealthCareApp/HealthCareAppBackend/src/main/java/com/healthcareapp/healthcareappbackend/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.healthcareapp.healthcareappbackend.controller;
+
+import com.healthcareapp.healthcareappbackend.dto.AccountDto;
+import com.healthcareapp.healthcareappbackend.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/account")
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+
+    @PutMapping(path = "/getAccountByIdCard")
+    public AccountDto getAccountByIdCard(@RequestBody AccountDto accountDtoInput) {
+        AccountDto account = accountService.findAccountByIdCard(accountDtoInput.getIdCard());
+
+        if (!account.getPassword().equals(accountDtoInput.getPassword())) {
+            return null;
+        }
+
+        return account;
+    }
+
+    @PostMapping(path = "/signup")
+    public AccountDto signUp(@RequestBody AccountDto accountDtoInput) {
+        return accountService.save(accountDtoInput);
+    }
+}
---
+++ HealthCareApp/HealthCareAppBackend/src/main/java/com/healthcareapp/healthcareappbackend/service/AccountService.java
@@ -0,0 +1,44 @@
+package com.healthcareapp.healthcareappbackend.service;
+
+import com.healthcareapp.healthcareappbackend.converter.AccountMapper;
+import com.healthcareapp.healthcareappbackend.dto.AccountDto;
+import com.healthcareapp.healthcareappbackend.entity.AccountEntity;
+import com.healthcareapp.healthcareappbackend.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    public AccountDto findAccountByIdCard(String idCard) {
+        Optional<AccountEntity> account = accountRepository.findByIdCard(idCard);
+
+        if (account.isEmpty()) {
+            return null;
+        }
+
+        AccountEntity accountEntity = account.get();
+
+        return accountMapper.INSTANCE.accountEntityToAccountDto(accountEntity);
+    }
+
+    public AccountDto save(AccountDto accountDto) {
+        // Already registered with this idCard
+        if (accountRepository.findByIdCard(accountDto.getIdCard()).isPresent()) {
+            return null;
+        }
+
+        // Create new record
+        AccountEntity accountEntity = accountMapper.INSTANCE.accountDtoToAccountEntity(accountDto);
+        accountEntity = accountRepository.save(accountEntity);
+
+        return accountMapper.INSTANCE.accountEntityToAccountDto(accountEntity);
+    }
+}
