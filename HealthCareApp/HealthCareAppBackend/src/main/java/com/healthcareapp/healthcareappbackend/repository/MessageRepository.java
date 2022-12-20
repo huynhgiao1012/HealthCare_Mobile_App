36266ee2c9f3122254e549f08ef6a3e367784a4a
DuongMinhLoc <duongminhloc201@gmail.com>
2022-12-20 19:55:31 +0700

    Delete backend folder in HealthCareApp

--- HealthCareApp/HealthCareAppBackend/src/main/java/com/healthcareapp/healthcareappbackend/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthcareapp.healthcareappbackend.repository;
-
-import com.healthcareapp.healthcareappbackend.entity.MessageEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<MessageEntity, Long> {
-}
